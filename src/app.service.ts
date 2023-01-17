c0e96f50de7eb388947c25430413b9615a60db46
Henrique Nery <henriquenery07@gmail.com>
2023-01-17 13:23:25 -0300

    feat: return all informations of a Lenovo notebooks

--- src/app.service.ts
+++ src/app.service.ts
@@ -6,24 +6,29 @@ export class AppService {
   public url: string =
     'https://webscraper.io/test-sites/e-commerce/allinone/computers/laptops';
 
-  async getAmountNotebooks(): Promise<number> {
+  async getAmountNotebooks(): Promise<any > {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
     await page.goto(this.url);
 
     await page.waitForSelector('.thumbnail');
 
+    const informationNotebooks = await page.$$eval('.thumbnail', (el) =>
+      el.map((elem) => elem.innerHTML),
+    );
+
     const notebooksTitle = await page.$$eval('.thumbnail a', (el) =>
       el.map((elem) => elem.textContent),
     );
     let indexLenovoNotebooks = -1;
+    let allInformationLenovoNotebooks: Array<string> = []
     for (let i = 0; i < notebooksTitle.length; i++) {
       if (notebooksTitle[i].includes('Lenovo')) {
-        indexLenovoNotebooks = i;
+        allInformationLenovoNotebooks.push(notebooksTitle[i]);
       }
     }
 
     await browser.close();
-    return notebooksTitle.length;
+    return allInformationLenovoNotebooks;
   }
 }
