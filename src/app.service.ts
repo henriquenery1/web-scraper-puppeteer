8e54d2b9f6bfe6429b3eb82a3772f2b0f1db5ed4
Henrique Nery <henriquenery07@gmail.com>
2023-01-17 12:36:13 -0300

    feat: function to pick up number of notebooks

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import puppeteer from 'puppeteer';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  public url: string =
+    'https://webscraper.io/test-sites/e-commerce/allinone/computers/laptops';
+
+  async getAmountNotebooks(): Promise<number> {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto(this.url);
+
+    await page.waitForSelector('.thumbnail');
+
+    const notebooksTitle = await page.$$eval(".thumbnail a", (el) =>
+    el.map((elem) => elem.textContent)
+  );
+
+    await browser.close();
+    return notebooksTitle.length;
   }
 }
